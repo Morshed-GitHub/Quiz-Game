8b84cfb28cfd2edf08edd84c92f1d7e2005191e5
morshed <morshedraian12345@gmail.com>
2023-09-04 03:01:35 +0600

    Splash Screen Completed

--- app/src/main/java/com/learning/quizgameproject/MainActivity.kt
+++ app/src/main/java/com/learning/quizgameproject/MainActivity.kt
@@ -2,10 +2,18 @@ package com.learning.quizgameproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.learning.quizgameproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view : View = mainBinding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/learning/quizgameproject/WelcomeActivity.kt
+++ app/src/main/java/com/learning/quizgameproject/WelcomeActivity.kt
@@ -1,11 +1,35 @@
 package com.learning.quizgameproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatDelegate
+import com.learning.quizgameproject.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    lateinit var splashBinding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        splashBinding = ActivityWelcomeBinding.inflate(layoutInflater)
+        val view : View = splashBinding.root
+        setContentView(view)
+
+        val alphaAnimation : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.splash_anim) // context, animation_id
+        splashBinding.tvSplash.startAnimation(alphaAnimation)
+
+        // In order to handle time delaying
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({ // object : Runnable {} -> Short
+            val intent : Intent = Intent(this@WelcomeActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Destroy the current activity
+        }, 4000) // 5000 milliseconds -> 5 seconds
     }
 }
