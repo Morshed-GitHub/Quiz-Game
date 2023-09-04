599eb44e37b889de9c50a9651c6ff6919f582a9a
morshed <morshedraian12345@gmail.com>
2023-09-05 04:50:55 +0600

    Question Page Design Completed

--- app/src/main/java/com/learning/quizgameproject/QuizActivity.kt
+++ app/src/main/java/com/learning/quizgameproject/QuizActivity.kt
@@ -2,10 +2,23 @@ package com.learning.quizgameproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.learning.quizgameproject.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
+    lateinit var quizBinding: ActivityQuizBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
+        quizBinding = ActivityQuizBinding.inflate(layoutInflater)
+        val view : View = quizBinding.root
+        setContentView(view)
+
+        quizBinding.buttonNext.setOnClickListener {  }
+        quizBinding.buttonFinish.setOnClickListener {  }
+
+        quizBinding.tvOptA.setOnClickListener {  }
+        quizBinding.tvOptB.setOnClickListener {  }
+        quizBinding.tvOptC.setOnClickListener {  }
+        quizBinding.tvOptD.setOnClickListener {  }
     }
 }
