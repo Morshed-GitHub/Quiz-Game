321e2534cae0c30ac28c0afa31a15323e083af43
morshed <morshedraian12345@gmail.com>
2023-09-05 03:35:00 +0600

    Home Screen Completed
    Worked Done
    - Google signout functionality added

--- app/src/main/java/com/learning/quizgameproject/MainActivity.kt
+++ app/src/main/java/com/learning/quizgameproject/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.learning.quizgameproject.databinding.ActivityMainBinding
 
@@ -19,10 +23,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         mainBinding.buttonSignOut.setOnClickListener {
+            // Email & Password Login Case
             FirebaseAuth.getInstance().signOut()
+
+            // Google Sign In Case
+            val gso : GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build()
+            val googleSignInClient : GoogleSignInClient = GoogleSignIn.getClient(this, gso)
+            googleSignInClient.signOut().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                  Toast.makeText(applicationContext, "Successfully logged out ✔", Toast.LENGTH_SHORT).show()
+                }
+            }
+
             val intent : Intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        mainBinding.buttonStartQuiz.setOnClickListener {
+            val intent : Intent = Intent(this@MainActivity, QuizActivity::class.java)
+            startActivity(intent)
         }
     }
 }
