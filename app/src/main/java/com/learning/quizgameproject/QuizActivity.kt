89eabb9f11e39ed7d0ec43fb07a4d4fff1f1a0f9
morshed <morshedraian12345@gmail.com>
2023-09-05 08:58:15 +0600

    Get & Show Questions From Firebase Database

--- app/src/main/java/com/learning/quizgameproject/QuizActivity.kt
+++ app/src/main/java/com/learning/quizgameproject/QuizActivity.kt
@@ -3,22 +3,75 @@ package com.learning.quizgameproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.firebase.database.*
 import com.learning.quizgameproject.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
     lateinit var quizBinding: ActivityQuizBinding
+    private val db : FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val dbRef : DatabaseReference = db.reference.child("questions")
+
+    var question : String = ""
+    var optA : String = ""
+    var optB : String = ""
+    var optC : String = ""
+    var optD : String = ""
+    var answer : String = ""
+    var questionCount : Int = 0
+    var questionNumber : Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         quizBinding = ActivityQuizBinding.inflate(layoutInflater)
         val view : View = quizBinding.root
         setContentView(view)
 
-        quizBinding.buttonNext.setOnClickListener {  }
+        gameLogic()
+
+        quizBinding.buttonNext.setOnClickListener { gameLogic() }
         quizBinding.buttonFinish.setOnClickListener {  }
 
         quizBinding.tvOptA.setOnClickListener {  }
         quizBinding.tvOptB.setOnClickListener {  }
         quizBinding.tvOptC.setOnClickListener {  }
         quizBinding.tvOptD.setOnClickListener {  }
+    }
+
+    private fun gameLogic() {
+        dbRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                questionCount = snapshot.childrenCount.toInt()
+
+                if (questionNumber <= questionCount) {
+                    question = snapshot.child(questionNumber.toString()).child("q").value.toString()
+                    optA = snapshot.child(questionNumber.toString()).child("a").value.toString()
+                    optB = snapshot.child(questionNumber.toString()).child("b").value.toString()
+                    optC = snapshot.child(questionNumber.toString()).child("c").value.toString()
+                    optD = snapshot.child(questionNumber.toString()).child("d").value.toString()
+                    answer = snapshot.child(questionNumber.toString()).child("answer").value.toString()
+
+                    quizBinding.tvQuestion.text = question
+                    quizBinding.tvOptA.text = optA
+                    quizBinding.tvOptB.text = optB
+                    quizBinding.tvOptC.text = optC
+                    quizBinding.tvOptD.text = optD
+
+                    quizBinding.progressBarQuiz.isVisible = false
+                    quizBinding.linearLayoutInfo.isVisible = true
+                    quizBinding.linearLayoutQuestion.isVisible = true
+                    quizBinding.linearLayoutbutton.isVisible = true
+
+                    questionNumber++
+                } else {
+                    Toast.makeText(applicationContext, "Congratulations!! you have successfully completed the Quiz Game ✔", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, error.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
